5031be8e1b7e294ffef5e92b55e41e38adc806ce
Brandon Chinn <brandon@leapyear.io>
2018-05-11 17:49:56 -0700

    Use postBuild instead of postCopy

--- example/Setup.hs
+++ example/Setup.hs
@@ -2,7 +2,7 @@ import Codec.SelfExtract.Distribution (bundle)
 import Distribution.Simple
 
 main = defaultMainWithHooks simpleUserHooks
-  { postCopy = \args cf pd lbi -> do
-      postCopy simpleUserHooks args cf pd lbi
+  { postBuild = \args bf pd lbi -> do
+      postBuild simpleUserHooks args bf pd lbi
       bundle "foo" "dist" lbi
   }
