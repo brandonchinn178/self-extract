72a945b726cbc6a385cad0f7862a09a2652e48b8
Brandon Chinn <brandon@leapyear.io>
2018-05-14 13:01:26 -0700

    Move example to example/basic

--- example/basic/Setup.hs
+++ example/basic/Setup.hs
@@ -4,5 +4,5 @@ import Distribution.Simple
 main = defaultMainWithHooks simpleUserHooks
   { postBuild = \args bf pd lbi -> do
       postBuild simpleUserHooks args bf pd lbi
-      bundle "foo" "dist" lbi
+      bundle "self-extract-basic" "dist" lbi
   }
