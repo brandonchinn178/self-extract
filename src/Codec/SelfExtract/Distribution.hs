7b058d827b35320eba3ab31683c34f256cac9106
Brandon Chinn <brandonchinn178@gmail.com>
2018-06-20 12:11:28 -0700

    Update to 0.3.0 (#12)
    * Update to 0.3.0
    
    * Fix docs

--- src/Codec/SelfExtract/Distribution.hs
+++ src/Codec/SelfExtract/Distribution.hs
@@ -21,6 +21,7 @@ import Distribution.Simple.Setup (ConfigFlags(..), fromFlag)
 import Path (Abs, File, Path, parseAbsDir, parseRelFile, toFilePath, (</>))
 
 -- | Get the executable with the given name with the given LocalBuildInfo.
+--
 -- @
 -- main = defaultMainWithHooks simpleUserHooks
 --   { postCopy = \args cf pd lbi -> do
@@ -33,12 +34,13 @@ getExe :: LocalBuildInfo -> String -> IO FilePath
 getExe lbi exeName = toFilePath <$> getExe' lbi exeName
 
 -- | Same as 'getExe', except using the 'Path' library.
+--
 -- @
 -- main = defaultMainWithHooks simpleUserHooks
 --   { postCopy = \args cf pd lbi -> do
 --       postCopy simpleUserHooks args cf pd lbi
 --       exe <- getExe' lbi "name-of-executable"
---       bundle exe [reldir|.\/static\/|]
+--       bundle' exe [reldir|.\/static\/|]
 --   }
 -- @
 getExe' :: LocalBuildInfo -> String -> IO (Path Abs File)
